fa6b98d2ae988f321d1e5c9dddbbf1eec947d337
眼神空洞的大學生 <beomqbear0313@gmail.com>
2026-03-27 11:12:49 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Test
+{
+	private int Mid;
+	private int Final;
+
+	public Test(int Mid, int Final)
+	{
+		this.Mid = Mid;
+		this.Final = Final;
+	}
+
+	public void SetMid(int value)
+	{
+		this.Mid = value;
+    }
+
+	public void SetFinal(int value)
+    {
+		this.Final = value;
+    }
+
+
+	public int GetMid()
+	{
+		return this.Mid;
+    }
+
+	public int GetFinal()
+	{
+		return Final;
+    }
+
+	public double GetAvg()
+	{
+		return (Mid + Final) / 2.0;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _03273
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test std1 = new Test(78, 72);
+            Test std2 = new Test(5, 5);
+
+            std1.SetMid(88);
+            std2.SetFinal(82);
+
+            Console.WriteLine("1:" + std1.GetAvg().ToString());
+            Console.WriteLine("2:" + std2.GetAvg().ToString());
+        }
+    }
+}
